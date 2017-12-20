357fbc655646d78a25d1950c20a343110ff819d4
Nahom Tsadu <nahomtsadu@gmail.com>
2017-12-20 10:32:59 -0500

    Let's get started!
    Signed-off-by: Nahom Tsadu <nahomtsadu@gmail.com>

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { NewRequestPage } from '../pages/employee-page/new-request/new-request';
+import { PendingRequestsPage } from '../pages/employee-page/pending-requests/pending-requests';
+import { ProcessedRequestsPage } from '../pages/employee-page/processed-requests/processed-request';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component' 
+import { Routes } from '@angular/router';
+
+export const appRoutes : Routes = [
+    {   path: '', 
+        redirectTo: 'login', 
+        pathMatch: 'full' 
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    }
+];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,75 @@
+import { Component, Input, Output, EventEmitter, NgZone, OnInit } from '@angular/core';
+import { ERSController } from '../../providers/ers-controller/ers-controller';
+import { MaterializeDirective } from "angular2-materialize";
+import { MatTabChangeEvent } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
+import { setTimeout } from 'timers';
+import { Router } from "@angular/router";
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  @Output() loginEvent: EventEmitter<any> = new EventEmitter<any>();
+
+  username : string;
+  password : string;
+  loginDone : boolean = false;
+  loginType : number = 0;
+
+  tabChanged : Function = (tabChangeEvent: MatTabChangeEvent): void => {
+    this.loginType = tabChangeEvent.index;
+    console.log('Login Type => ', this.loginType);
+  }
+
+  promiseEmployeeLogin : Function = ()=> {
+    new Promise((resolve, reject) => {
+      if(this.ersApp.currentUser != null) resolve();
+      else reject();
+      }).then(()=>{this.ersApp.initializeEmployee(this.loginEvent);}, 
+              ()=>{console.log("Employee Login Failed!");});
+  }
+
+  promiseManagerLogin : Function = ()=> {
+    new Promise((resolve, reject) => {
+      if(this.ersApp.currentUser != null) resolve();
+      else reject();
+      }).then(()=>{this.ersApp.initializeManager(this.loginEvent);}, 
+              ()=>{console.log("Manager Login Failed!");});
+  }
+
+  constructor(public ersApp : ERSController, public router: Router, public snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.ersApp.currentUser = {firstname: "Jon", lastName: "Doe"};
+  }
+
+  openSnackBar(message:string) {
+    this.snackBar.open(message, "OK", {
+      duration: 2000,
+    });
+  }
+
+  login(){
+    // if(this.loginType == 0) this.ersApp.loginEmployee(this.username, this.password, this.promiseEmployeeLogin, this.openSnackBar);
+    // else if(this.loginType == 1) this.ersApp.loginManager(this.username, this.password, this.promiseManagerLogin);
+    if(this.loginType == 0) this.router.navigate(["dashboard"]);
+    else if(this.loginType == 1) console.log("Out of service..");
+  }
+
+  register(){
+    this.router.navigate(["register"]);
+  }
+
+  updateUsername(u : any){
+    this.username = u;
+  }
+
+  updatePassword(p : any){
+    this.password = p;
+  }
+}
